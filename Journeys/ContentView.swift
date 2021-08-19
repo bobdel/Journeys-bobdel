07fba1b359f108abb8a2b21047be11a28d8f6805
Robert DeLaurentis <lists@bobdel.com>
2021-08-19 10:18:24 -0700

    Reset bundle id and made it capable of building and running.

--- Journeys/ContentView.swift
+++ Journeys/ContentView.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
- Your company has been asked to build a proof of concept for a travel company – if the prototype gets the green light then you'll be paid an absurd amount of money to build more or less the same thing, just with fewer bugs. (Probably)
- Your CTO put together the bare bones of the app, but it's down to you to fill it out with lots of SwiftUI code from iOS 14.
+// Your company has been asked to build a proof of concept for a travel company – if the prototype gets the green light then you'll be paid an absurd amount of money to build more or less the same thing, just with fewer bugs. (Probably)
+// Your CTO put together the bare bones of the app, but it's down to you to fill it out with lots of SwiftUI code from iOS 14.
 
 struct ContentView: View {
     @EnvironmentObject var locations: Locations
