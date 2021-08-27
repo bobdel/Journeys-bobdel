f6fe826fb4c46e1286feb4138d2c453e75998bdc
Robert DeLaurentis <lists@bobdel.com>
2021-08-27 16:14:58 -0700

    Added scaled metric property wrapper to scale map annotations

--- Journeys/LocationsView.swift
+++ Journeys/LocationsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct LocationsView: View {
     @EnvironmentObject var locations: Locations
+    @ScaledMetric var annotationSize: CGFloat = 80.0
 
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
 
@@ -20,12 +21,12 @@ struct LocationsView: View {
                     Image(location.country)
                         .renderingMode(.original)
                         .resizable()
-                        .frame(width: 80, height: 40)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
                                 .stroke(Color.blue, lineWidth: 4)
                         )
+                        .frame(width: max(80, annotationSize), height: max(40, annotationSize / 2))
                 }
             }
         }
